fff9bd39803732b9254b5c0c73076386b8e56a91
Damian Mendel <mendeldamian01@gmail.com>
2022-04-27 22:35:59 +0200

    explicit form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faTiktok, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { ParentsInfoComponent } from './parents-info/parents-info.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     QuizComponent,
-    ParentsInfoComponent
+    ParentsInfoComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faTiktok,
+      faFacebook,
+      faInstagram
+    );
+  }
+}
