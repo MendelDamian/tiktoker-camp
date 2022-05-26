c3102b15830442df0129d6f6229ec4cb0ed08731
Blindeen <kuba.szmajda@gmail.com>
2022-05-26 21:34:09 +0200

    Changes by mende

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -8,6 +8,95 @@ import {isPhonenumber, isName, validEmail} from "../../mixins/functions";
 })
 export class FormComponent {
 
-  constructor() { }
+  isValid: boolean = false;
+
+  formValid: FormValid = {
+    firstName: true,
+    lastName: true,
+    email: true,
+    phone: true,
+  }
+
+  formData: FormData = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+  }
+
+  constructor() {
+  }
+
+  firstNameValid(): string {
+    this.isValid = false;
+    if (!this.formData.firstName.length) {
+      return "";
+    }
+
+    if (!isName(this.formData.firstName)) {
+      return "is-invalid";
+    }
+
+    this.isValid = true;
+    return "is-valid";
+  }
+
+  lastNameValid(): string {
+    this.isValid = false;
+    if (!this.formData.lastName.length) {
+      return "";
+    }
+
+    if (!isName(this.formData.lastName)) {
+      return "is-invalid";
+    }
+
+    this.isValid = true;
+    return "is-valid"
+  }
+
+  emailValid(): string {
+    this.isValid = false;
+    if (!this.formData.email.length) {
+      return "";
+    }
+
+    if (!validEmail(this.formData.email)) {
+      return "is-invalid";
+    }
+
+    this.isValid = true;
+    return "is-valid";
+  }
+
+  numberValid(): string {
+    this.isValid = false;
+    if (!this.formData.phone.length) {
+      return "";
+    }
+
+    if (!isPhonenumber(this.formData.phone)) {
+      return "is-invalid";
+    }
+
+    this.isValid = true;
+    return "is-valid";
+  }
+
 
 }
+
+export interface FormValid {
+  firstName: boolean,
+  lastName: boolean,
+  email: boolean,
+  phone: boolean,
+}
+
+export interface FormData {
+  firstName: string,
+  lastName: string,
+  email: string,
+  phone: string,
+}
+
