3330947658ad5b1a5baeeb1d984bb01f14a45a8c
Damian Mendel <mendeldamian01@gmail.com>
2022-05-31 12:28:19 +0200

    updated validating method

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
-import { validateEmail, validateName, validatePhoneNumber } from '../../mixins/functions';
 import { DatabaseService } from '../database.service';
+import {
+  validateEmail,
+  validateLettersOnly,
+  validateMaxLength,
+  validateMinLength,
+  validateNonEmpty,
+  validateNumbersOnly,
+  validateStartsWithUpperCase
+} from '../../mixins/functions';
 
 @Component({
   selector: 'app-form',
@@ -8,41 +16,135 @@ import { DatabaseService } from '../database.service';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
-  createuser_err: ValidationState = ValidationState.none;
+  validationState = ValidationState; // For using in template
+  createUserErr: ValidationState = ValidationState.none;
   loading: boolean = false;
 
   formData: FormData = {
     firstName: {
       value: '',
       valid: ValidationState.none,
-      errorMessages: [
-        'Pole wymagane.',
-        'Imię musi się zaczynać od wielkiej litery.',
+      validation: [
+        {
+          validator: validateMinLength,
+          n: 2,
+          state: ValidationState.none,
+          errorMessage: 'Imię jest za krótkie.'
+        },
+        {
+          validator: validateMaxLength,
+          n: 32,
+          state: ValidationState.none,
+          errorMessage: 'Imię jest za długie.'
+        },
+        {
+          validator: validateLettersOnly,
+          state: ValidationState.none,
+          errorMessage: 'Imię może zawierać tylko litery.'
+        },
+        {
+          validator: validateStartsWithUpperCase,
+          state: ValidationState.none,
+          errorMessage: 'Imię musi zaczynać się z dużej litery.'
+        },
+        // {
+        //   validator: validateUpperCaseOnlyOnBeginning,
+        //   state: ValidationState.none,
+        //   errorMessage: 'Imię jest nieprawidłowe.'
+        // },
+        {
+          validator: validateNonEmpty,
+          state: ValidationState.none,
+          errorMessage: 'Pole wymagane.'
+        },
       ]
     },
     lastName: {
       value: '',
       valid: ValidationState.none,
-      errorMessages: [
-        'Pole wymagane.',
-        'Nazwisko musi się zaczynać od wielkiej litery.',
+      validation: [
+        {
+          validator: validateMinLength,
+          n: 2,
+          state: ValidationState.none,
+          errorMessage: 'Nazwisko jest za krótkie.'
+        },
+        {
+          validator: validateMaxLength,
+          n: 32,
+          state: ValidationState.none,
+          errorMessage: 'Nazwisko jest za długie.'
+        },
+        {
+          validator: validateLettersOnly,
+          state: ValidationState.none,
+          errorMessage: 'Nazwisko może zawierać tylko litery.'
+        },
+        {
+          validator: validateStartsWithUpperCase,
+          state: ValidationState.none,
+          errorMessage: 'Nazwisko musi zaczynać się z dużej litery.'
+        },
+        // {
+        //   validator: validateUpperCaseOnlyOnBeginning,
+        //   state: ValidationState.none,
+        //   errorMessage: 'Nazwisko jest nieprawidłowe.'
+        // },
+        {
+          validator: validateNonEmpty,
+          state: ValidationState.none,
+          errorMessage: 'Pole wymagane.'
+        },
       ]
     },
     email: {
       value: '',
       valid: ValidationState.none,
-      errorMessages: [
-        'Pole wymagane.',
-        'Niepoprawny adres email.',
+      validation: [
+        {
+          validator: validateMaxLength,
+          n: 128,
+          state: ValidationState.none,
+          errorMessage: 'Email jest za długi.'
+        },
+        {
+          validator: validateEmail,
+          state: ValidationState.none,
+          errorMessage: 'Email jest nieprawidłowy.'
+        },
+        {
+          validator: validateNonEmpty,
+          state: ValidationState.none,
+          errorMessage: 'Pole wymagane.'
+        },
       ]
     },
     phone: {
       value: '',
       valid: ValidationState.none,
-      errorMessages: [
-        'Pole wymagane.',
-        'Niepoprawny numer telefonu.',
-        'Numer powinien składać się z 9 cyfr.',
+      validation: [
+        {
+          validator: validateMinLength,
+          n: 9,
+          state: ValidationState.none,
+          errorMessage: 'Numer telefonu jest za krótki.'
+        },
+        {
+          validator: validateMaxLength,
+          n: 9,
+          state: ValidationState.none,
+          errorMessage: 'Numer telefonu jest za długi.'
+        },
+        {
+          validator: validateNumbersOnly,
+          state: ValidationState.none,
+          errorMessage: 'Numer telefonu jest nieprawidłowy.'
+        },
+        {
+          validator: validateNonEmpty,
+          state: ValidationState.none,
+          errorMessage: 'Pole wymagane.'
+        },
       ]
     },
   };
@@ -53,10 +155,10 @@ export class FormComponent {
   onClickHandler(): void {
     this.loading = true;
 
-    this.firstNameValid();
-    this.lastNameValid();
-    this.emailValid();
-    this.phoneValid();
+    this.validate('firstName');
+    this.validate('lastName');
+    this.validate('email');
+    this.validate('phone');
 
     // Check if all fields are valid
     if (this.formData.firstName.valid !== ValidationState.valid
@@ -67,67 +169,63 @@ export class FormComponent {
       return;
     }
 
-    this.createuser_err = ValidationState.none;
+    this.createUserErr = ValidationState.none;
     this.database.createUser(this.formData).subscribe(create_res => {
       if (create_res.id) {
         this.database.updateUser(create_res.id, this.formData).subscribe(update_res => {
           this.loading = false;
           if (update_res.err) {
-            this.createuser_err = ValidationState.invalid;
+            this.createUserErr = ValidationState.invalid;
           } else {
-            this.createuser_err = ValidationState.valid;
+            this.clearForm();
+            this.createUserErr = ValidationState.valid;
           }
         });
       } else {
         this.loading = false;
-        this.createuser_err = ValidationState.invalid;
+        this.createUserErr = ValidationState.invalid;
       }
     });
+  }
 
+  clearForm(): void {
     this.formData.firstName.value = '';
     this.formData.lastName.value = '';
     this.formData.email.value = '';
     this.formData.phone.value = '';
 
-    this.formData.firstName.valid = 0;
-    this.formData.lastName.valid = 0;
-    this.formData.email.valid = 0;
-    this.formData.phone.valid = 0;
+    this.formData.firstName.valid = ValidationState.none;
+    this.formData.lastName.valid = ValidationState.none;
+    this.formData.email.valid = ValidationState.none;
+    this.formData.phone.valid = ValidationState.none;
 
+    this.formData.firstName.validation.forEach(validation => {
+      validation.state = ValidationState.none;
+    });
+    this.formData.lastName.validation.forEach(validation => {
+      validation.state = ValidationState.none;
+    });
+    this.formData.email.validation.forEach(validation => {
+      validation.state = ValidationState.none;
+    });
+    this.formData.phone.validation.forEach(validation => {
+      validation.state = ValidationState.none;
+    });
   }
 
-  firstNameValid(): void {
-    if (!this.formData.firstName.value.length || !validateName(this.formData.firstName.value)) {
-      this.formData.firstName.valid = ValidationState.invalid;
-      return;
-    }
+  validate(field: string): void {
+    this.formData[field].valid = ValidationState.none;
+    this.formData[field].validation.forEach((validation) => {
+      validation.state = validation.validator(this.formData[field].value, validation.n)
+        ? ValidationState.valid : ValidationState.invalid;
 
-    this.formData.firstName.valid = ValidationState.valid;
-  }
+      if (validation.state === ValidationState.invalid) {
+        this.formData[field].valid = ValidationState.invalid;
+      }
+    });
 
-  lastNameValid(): void {
-    if (!this.formData.lastName.value.length || !validateName(this.formData.lastName.value)) {
-      this.formData.lastName.valid = ValidationState.invalid;
-      return;
-    }
-
-    this.formData.lastName.valid = ValidationState.valid;
-  }
-
-  emailValid(): void {
-    if (!this.formData.email.value.length || !validateEmail(this.formData.email.value)) {
-      this.formData.email.valid = ValidationState.invalid;
-      return;
-    }
-
-    this.formData.email.valid = ValidationState.valid;
-  }
-
-  phoneValid(): void {
-    if (!this.formData.phone.value.length || !validatePhoneNumber(this.formData.phone.value)) {
-      this.formData.phone.valid = ValidationState.invalid;
-    } else {
-      this.formData.phone.valid = ValidationState.valid;
+    if (this.formData[field].valid === ValidationState.none) {
+      this.formData[field].valid = ValidationState.valid;
     }
   }
 
@@ -138,12 +236,21 @@ export interface FormData {
   lastName: InputField,
   email: InputField,
   phone: InputField,
+
+  [propName: string]: InputField;
 }
 
 interface InputField {
   value: string,
   valid: ValidationState,
-  errorMessages: Array<string>,
+  validation: Array<ValidationField>,
+}
+
+interface ValidationField {
+  validator: (fieldValue: string, n?: number) => boolean,
+  n?: number,
+  state: ValidationState,
+  errorMessage: string
 }
 
 enum ValidationState {
--- src/mixins/functions.ts
+++ src/mixins/functions.ts
@@ -2,14 +2,34 @@ export function delay(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export function validateName(char: string): boolean {
+export function validateUpperCaseOnlyOnBeginning(char: string, n: (number | undefined)): boolean {
   return /^[A-Z][a-z]*$/.test(char);
 }
 
-export function validatePhoneNumber(number: string) {
-  return /\d{9}$/.test(number);
+export function validateEmail(str: string, n: (number | undefined)): boolean {
+  return /(.+)@(.+){2,}\.(.+){2,}/.test(str);
 }
 
-export function validateEmail(str: string): boolean {
-  return /(.+)@(.+){2,}\.(.+){2,}/.test(str);
+export function validateStartsWithUpperCase(str: string, n: (number | undefined)): boolean {
+  return /^[A-Z]/.test(str);
+}
+
+export function validateMinLength(str: string, n: (number | undefined)): boolean {
+  return n === undefined || str.length >= n;
+}
+
+export function validateMaxLength(str: string, n: (number | undefined)): boolean {
+  return n === undefined || str.length <= n;
+}
+
+export function validateLettersOnly(str: string, n: (number | undefined)): boolean {
+  return /^[a-zA-Z]+$/.test(str);
+}
+
+export function validateNumbersOnly(str: string, n: (number | undefined)): boolean {
+  return /^\d+$/.test(str);
+}
+
+export function validateNonEmpty(str: string, n: (number | undefined)): boolean {
+  return str.length > 0;
 }
